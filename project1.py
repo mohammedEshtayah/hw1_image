bada5a86a7e42cfa49e6ccac2e8feebcdbe96baf
mohammedeshtayah <abualyosr96@hotmail.com>
2019-07-05 01:16:06 +0300

    finish

--- project1.py
+++ project1.py
@@ -1,6 +1,7 @@
 import cv2  
 import numpy as np;
 import math
+from matplotlib import pyplot as plt 
 im=cv2.imread('3(a).tif',0)
 #print('{}'.format(a.shape[0])
 
@@ -19,20 +20,24 @@ def  power_transformation():
     c=1
     cv2.imshow('image',c* np.power(im,y))
     cv2.waitKey(0)
+def calc_Histogram():
+    histr = cv2.calcHist([im],[0],None,[256],[0,256]) 
+    plt.plot(histr) 
+    plt.show()  
+def equalize_Hist():
+    img = cv2.imread('3(a).tif')
+    img_yuv = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
+    img_yuv[:,:,0] = cv2.equalizeHist(img_yuv[:,:,0])
+    img_output = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR)
+    cv2.imshow('Histogram equalized', img_output)
 
+    cv2.waitKey(0)   
 def main():
     #log_transformation()
-    power_transformation()
-    print("fac")
-    print("aaa")
-    #print(im[45][80])
-    #item[]
-    #for i in range(0, 10):
-    #    for j in range(0,10):
-    #        item[i][j]=image[i][j]
-
-    #cv2.imshow('image',item)
-    #print("new {}".format(im[1]))
+    #power_transformation()
+    #calc_Histogram() 
+    equalize_Hist()
+  
  
 
 
